8e7700a790b2f4d6340b7c65597be870cdb03065
Paras Rawat <parasrawat67@gmail.com>
2021-05-19 16:54:45 +0530

    Action Results

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,8 +12,10 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return Content("Hello World!!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { Page = 1, SortBy = "name" });
         }
     }
 }
