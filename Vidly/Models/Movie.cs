eaa4e93330ed643801320811dde4d3ee3f32d920
Paras Rawat <parasrawat67@gmail.com>
2021-05-21 08:56:25 +0530

    Adding Display Attribute To Movie Class for fixing labelfor in New View

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,14 +9,24 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(30)]
         public string Name { get; set; }
+
+        [Display(Name ="Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name ="Date Added")]
         public DateTime DateAdded { get; set; }
+
+        [Display(Name="Number In Stock")]
         public byte NumberInStock { get; set; }
+
         [Required]
         public Genre Genre { get; set; }
+
+        [Display(Name ="Genre")]
         public byte GenreId { get; set; }
     }
 }
