d9a646e5f43b0c015257adf3c3a68c7877c38564
Paras Rawat <parasrawat67@gmail.com>
2021-05-21 17:54:32 +0530

    Creating Web Api

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
 using System.Web.Mvc;
 using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace Vidly
 {
@@ -16,6 +17,7 @@ namespace Vidly
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
