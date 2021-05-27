00da1545746689fee3c9024cf574721b05e7ba3c
Paras Rawat <parasrawat67@gmail.com>
2021-05-27 18:03:55 +0530

    Adding And Fixing IdentityFramework Problem

--- Vidly/Models/ApplicationDbContext.cs
+++ Vidly/Models/ApplicationDbContext.cs
@@ -1,20 +1,42 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Vidly.Models
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationUser : IdentityUser
     {
-        public ApplicationDbContext():base("DefaultConnection")
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            :base("DefaultConnection", throwIfV1Schema: false)
         {
 
         }
+      
+
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
     }
 }
