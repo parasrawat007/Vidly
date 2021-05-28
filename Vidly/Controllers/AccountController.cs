f62d995ef4a4c738aae2e5719292509d24d04127
Paras Rawat <parasrawat67@gmail.com>
2021-05-28 15:41:27 +0530

    Creating Roles And Seeding Users And Roles

--- Vidly/Controllers/AccountController.cs
+++ Vidly/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
@@ -156,13 +157,23 @@ namespace Vidly.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    //await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    
+
+                    //Temp Code for Role
+                    //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    //var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                    //await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
+
+                    //await UserManager.AddToRoleAsync(user.Id, "CanManageMovies");
+
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
-                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    //string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("Index", "Home");
                 }
---
+++ Vidly/Migrations/202105280954019_SeedUsers.cs
@@ -0,0 +1,22 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedUsers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"INSERT INTO[dbo].[AspNetUsers]([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES(N'635f9966-e486-4968-b569-2bcd8e0dbb44', N'admin@vidly.com', 0, N'ALjbdeLeJpwptyzgR6/StyJTvc3MkzP9pvsmN0cr9e8caV/xpWcGR0aq6vSn/Jxajg==', N'20350f9c-e363-45e4-9e42-e50ef0ba3dab', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+                  INSERT INTO[dbo].[AspNetUsers]([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES(N'78a0218b-2081-4687-a5cb-3b09ccfe3238', N'guest@vidly.com', 0, N'AB25eXbxtS5h36sNDMohePRv4EBXfVDRBDFAVjJKE9qCIMBN0UFPtt6YfA9WKA71vw==', N'da9ccbd3-aebc-4f38-a53b-12b41cfecb65', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')");
+
+            Sql(@"INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'70481664-2be1-4699-8db9-3dda1fbf1678', N'CanManageMovies')");
+
+            Sql(@"INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'635f9966-e486-4968-b569-2bcd8e0dbb44', N'70481664-2be1-4699-8db9-3dda1fbf1678')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
