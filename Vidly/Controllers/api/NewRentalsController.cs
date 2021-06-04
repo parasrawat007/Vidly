0eeda0c346557befc33f13524b44b3febc34c049
Paras Rawat <parasrawat67@gmail.com>
2021-06-04 15:55:31 +0530

    Edge Cases: Optimistic Approach

--- Vidly/Controllers/api/NewRentalsController.cs
+++ Vidly/Controllers/api/NewRentalsController.cs
@@ -25,20 +25,11 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            if (newRental.MovieIds.Count == 0)
-                return BadRequest("No Movie Ids have been given.");
-
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CunstomerId);
-
-            if (customer == null)
-                return BadRequest("Customer Id is not valid.");
-
+            var customer = _context.Customers.Single(c => c.Id == newRental.CunstomerId);
+   
             var movies = _context.Movies.Where(
                         m => newRental.MovieIds.Contains(m.Id)).ToList();
-
-            if (movies.Count != newRental.MovieIds.Count)
-                return BadRequest("One or more MovieIds are invalid.");
-
+   
             foreach (var movie in movies)
             {
                 if (movie.NumberAvailable == 0)
