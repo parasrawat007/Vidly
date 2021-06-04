21a4079e3fb4c524f6565895fb12039f41797adf
Paras Rawat <parasrawat67@gmail.com>
2021-06-04 15:01:06 +0530

    Adding RentalController Api

---
+++ Vidly/Controllers/api/NewRentalsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models.Identity_Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            return Ok();
+        }
+    }
+}
