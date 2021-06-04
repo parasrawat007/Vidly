90f474567b51c9cd34a88ce0de0f80331b4e97f6
Paras Rawat <parasrawat67@gmail.com>
2021-06-04 17:24:54 +0530

    Adding Auto-completion using typeahead

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -17,12 +17,13 @@ namespace Vidly
                         "~/Scripts/datatables/jquery.datatables.js",
                         "~/Scripts/datatables/datatables.bootstrap4.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/typeahead").Include(
+                        "~/Scripts/typeahead.bundle.js"));
+           // Use the development version of Modernizr to develop with and learn from. Then, when you're
+           // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
 
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+           bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootbox").Include(
@@ -34,7 +35,8 @@ namespace Vidly
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/Site.css",
-                      "~/Content/dataTables.bootstrap4.css"));
+                      "~/Content/dataTables.bootstrap4.css",
+                      "~/Content/typeahead.css"));
 
             //BundleTable.EnableOptimizations = true;
         }
